3ef556f9895ed86e1d212f22fc589b7f17a7b115
Megan Morris <megs@Mac-mini.local>
2021-03-24 11:03:12 -0400

    .

---
+++ config/userconfig.js
@@ -0,0 +1,9 @@
+const sqlCreds = {
+    user: "root",
+    password: "root",
+    host: "localhost",
+    port: 8889,
+    database: "roku_streaming"
+}
+
+module.exports = sqlCreds;
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const connect = require("../config/sqlconfig");
 
 router.get("/", (req, res) => {
     res.json({message: "you hit the api route"});
@@ -9,8 +10,30 @@ router.get("/users", (req, res) => {
     res.json({message: "you hit the users route"});
 })
 
+router.get("/movies", (req, res) => {
+    connect.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        connection.query('SELECT * FROM tbl_movies', function (error, results) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+       
+          res.json(results);
+        });
+      });
+})
+
 router.get("/movies/:id", (req, res) => {
-    res.json({ message: "all movies route", movie: req.params.id});
+    connect.query(`SELECT * FROM tbl_movies WHERE movies_id=${req.params.id}`, function (error, results, fields) {
+        if (error) throw error;
+        console.log("Results:", results, "fields:", fields);
+        res.json(results);
+    });
+
 })
 
 module.exports = router;
